bdeb0c55d140d263608cb54f098ef7fb50ebb9c1
Bui Toan <buitoan@Buis-MacBook-Pro.local>
2021-12-02 02:53:43 +0700

    update

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -5,7 +5,7 @@ module.exports = async function (deployer) {
   await deployer.deploy(NFTCollection);
 
   const deployedNFT =  await NFTCollection.deployed();
-  const NFTAddress = deployedNFT.address;
+  const NFTAddress = await deployedNFT.address;
   const serviceFee = 1000;
   await deployer.deploy(NFTMarketplace, NFTAddress, 1000);
 };
